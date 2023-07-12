63288c31066e8d4eb95b406ed541a4688241d1da
harshbix <kidabixson@gmail.com>
2023-07-13 01:08:20 +0300

    updated the register, made a variable

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bcrypt = require('bcrypt');
 
+const users = []
 //view engines and middleware
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => res.render('index'))
 
 app.get('/login', (req, res) => {
   res.render('login')
 })
+
 app.get('/register', (req, res) => {
-    res.render('register')
-  })
+  res.render('register')
+})
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push
+      (
+        {
+          id: Date.now().toString(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+        }
+      )
+      console.log(users);
+    res.redirect('/login')
+  } catch (error) {
+    res.redirect('/register')
+    console.log(error);
+  }
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
