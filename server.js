5138e901a1b2fc5cdc39af19d1320b0730ccae31
harshbix <kidabixson@gmail.com>
2023-07-12 23:19:13 +0300

    added view engines

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//view engines 
+//view engines and middleware
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+  })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
